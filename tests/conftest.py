ebfb136cedaad55b0e83d3048867e4065c848f5b
lazywizard15 <karthikben8@gmail.com>
2025-12-09 02:08:41 -0500

    changes made

--- tests/conftest.py
+++ tests/conftest.py
@@ -96,9 +96,7 @@ def setup_test_database(request):
     try:
         Base.metadata.drop_all(bind=test_engine)
         Base.metadata.create_all(bind=test_engine)
-        #init_db()
-        test_engine.dispose()
-        
+        init_db()
         logger.info("Test database initialized.")
     except Exception as e:
         logger.error(f"Error setting up test database: {str(e)}")
